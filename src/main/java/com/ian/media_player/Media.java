232ae7b6d0d41ba72ab17689485a54c4e75b7716
iankristoper <iankristoper26@gmail.com>
2024-03-30 22:46:09 +0800

    UPDATE: base class features

---
+++ src/main/java/com/ian/media_player/Media.java
@@ -0,0 +1,13 @@
+package com.ian.media_player;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+//super class for the sub classes
+public abstract class Media 
+{
+    //abstract method
+    public abstract void play();
+}
